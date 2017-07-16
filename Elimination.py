a8bb84f0560fc2fd30946494046850406bb56c1c
Nicole Cantrell <nikkicantrell@Nicoles-MacBook-Air.local>
2017-07-16 16:36:45 -0400

    Including some more tiebreaker rules

--- Elimination.py
+++ Elimination.py
@@ -1,5 +1,4 @@
 import sys
-import csv
 import argparse
 
 import openpyxl
@@ -50,8 +49,8 @@ class Elimination(object):
             self.rankEast()
             self.rankWest()
             if len(self.eliminationDates) < 14:
-                self.checkElimination('E', date)
-                self.checkElimination('W', date)
+                self.checkElimination('e', date)
+                self.checkElimination('w', date)
     def updateRecord(self, team, location, winner, game):
         """Updates the record of a team"""
         ind = self.eastTeamsIndexer.get(team, -1)
@@ -68,14 +67,14 @@ class Elimination(object):
                 self.westTeams[ind]['wins'] += 1
             else:
                 self.westTeams[ind]['losses'] += 1
-    def checkElimination(self, div, date):
+    def checkElimination(self, conf, date):
         """Checks if there is a team to be eliminated in the given conference"""
         teams = []
         eight = None
-        if div == "W" and self.westEliminated < 7:
+        if conf == "w" and self.westEliminated < 7:
             teams = self.westTeams
             eight = self.currentEightPosW
-        elif div == "E" and self.eastEliminated < 7:
+        elif conf == "e" and self.eastEliminated < 7:
             teams = self.eastTeams
             eight = self.currentEightPosE
         else:
@@ -86,12 +85,21 @@ class Elimination(object):
             if any(team['name'] in t for t in self.eliminationDates):
                 continue
             #this means there is no way the team can catch up to the eighth ranked team in their conference
-            if diffWins >= gamesLeft:
+            if diffWins > gamesLeft:
                 self.eliminationDates.append((team['name'], date))
-                if div == "W":
+                if conf == "w":
                     self.westEliminated += 1
                 else:
                     self.eastEliminated += 1
+            elif diffWins == gamesLeft:
+                #check if team could beat eight ranked in tiebreaker
+                tiebreakerLoser = self.breakTiebreaker(conf, team, eight, date)
+                if tiebreakerLoser[0] == team['name']:
+                    self.eliminationDates.append((team['name'], date))
+                    if conf == "w":
+                        self.westEliminated += 1
+                    else:
+                        self.eastEliminated += 1
     def rankEast(self):
         """Ranks the teams in the east conference from best to worst"""
         self.eastTeams = sorted(self.eastTeams, key=lambda k: k['losses'])
@@ -102,49 +110,59 @@ class Elimination(object):
         self.westTeams = sorted(self.westTeams, key=lambda k: k['losses'])
         self.currentEightPosW = self.westTeams[7]
         self.westTeamsIndexer = dict((t['name'], i) for i, t in enumerate(self.westTeams))
-    def breakTiebreaker(self, conf):
+    def breakTiebreaker(self, conf, first, second, date):
         """Breaks the tiebreaker in the given conference"""
         teams = []
+        firstInd = -1
+        secondInd = -1
         if conf == 'w':
-            self.rankWest()
             teams = self.westTeams
+            firstInd = self.westTeamsIndexer.get(first['name'], -1)
+            secondInd = self.westTeamsIndexer.get(second['name'], -1)
         else:
-            self.rankEast()
             teams = self.eastTeams
+            firstInd = self.eastTeamsIndexer.get(first['name'], -1)
+            secondInd = self.eastTeamsIndexer.get(second['name'], -1)
         #a tie breaker only needs to be broken if the teams are fighting for the eight spot
-        firstTeam = teams[7]
-        secondTeam = teams[8]
         totalGames = 0
         firstTeamWin = 0
         #calculates the teams record on only games they played against eachother
-        for game in firstTeam['games']:
-            if game['home'] == secondTeam['name']:
+        for game in first['games']:
+            if game['home'] == second['name']:
                 totalGames += 1
                 if game['winner'] == "Away":
                     firstTeamWin += 1
-            if game['away'] == secondTeam['name']:
+            if game['away'] == second['name']:
                 totalGames += 1
                 if game['winner'] == "Home":
                     firstTeamWin += 1
         if firstTeamWin < (totalGames - firstTeamWin):
-            self.eliminate(firstTeam, conf, 7)
+            return self.eliminate(first, conf, firstInd, date)
         elif firstTeamWin > (totalGames - firstTeamWin):
-            self.eliminate(secondTeam, conf, 8)
-    def eliminate(self, team, conf, ind):
+            return self.eliminate(second, conf, secondInd, date)
+        #finds if division leader
+        firstDiv = first['division']
+        secondDiv = second['division']
+        firstDivTeams = []
+        secondDivTeams = []
+        for team in teams:
+            if team['division'] == firstDiv:
+                firstDivTeams.append(team)
+            if team['division'] == secondDiv:
+                secondDivTeams.append(team)
+        firstDivTeams = sorted(firstDivTeams, key=lambda k: k['losses'])
+        secondDivTeams = sorted(secondDivTeams, key=lambda k: k['losses'])
+        if firstDivTeams[0] == first and secondDivTeams[0] != second:
+            return self.eliminate(second, conf, secondInd, date)
+        else: #secondDivTeams[0] == second and firstDivTeams[0] != first:
+            return self.eliminate(first, conf, firstInd, date)
+    def eliminate(self, team, conf, ind, date):
         """Eliminates a team that was a tiebreaker"""
-        self.eliminated_sheet.cell(row=self.row, column=1).value = team['name']
-        #since it was a tiebreaker, the date is the date of the last game
-        date = self.scores.cell(row=self.scores.max_row, column=1).value
         year = str(date.year)
         month = str(date.month)
         day = str(date.day)
         writeDate = month + '/' + day + '/' + year
-        self.eliminated_sheet.cell(row=self.row, column=2).set_explicit_value(value=writeDate, data_type=cell.Cell.TYPE_STRING)
-        self.row += 1
-        if conf == 'e':
-            del self.eastTeams[ind]
-        else:
-            del self.westTeams[ind]
+        return (team['name'], writeDate, ind)
     def writeEliminationDates(self):
         """Writes the elimination dates to the excel file"""
         self.generateTeamInfo()
@@ -181,9 +199,25 @@ class Elimination(object):
                 self.westTeamsIndexer = dict((t['name'], i) for i, t in enumerate(self.westTeams))
         #ensures there is no tiebreaker left
         if len(self.eastTeams) > 8:
-            self.breakTiebreaker('e')
+            self.rankEast()
+            first = self.eastTeams[7]
+            second = self.eastTeams[8]
+            date = self.scores.cell(row=self.scores.max_row, column=1).value
+            toElim = self.breakTiebreaker('e', first, second, date)
+            self.eliminated_sheet.cell(row=self.row, column=1).value = toElim[0]
+            self.eliminated_sheet.cell(row=self.row, column=2).set_explicit_value(value=toElim[1], data_type=cell.Cell.TYPE_STRING)
+            self.row += 1
+            del self.eastTeams[toElim[2]]
         if len(self.westTeams) > 8:
-            self.breakTiebreaker('w')
+            self.rankWest()
+            first = self.westTeams[7]
+            second = self.westTeams[8]
+            date = self.scores.cell(row=self.scores.max_row, column=1).value
+            toElim = self.breakTiebreaker('w', first, second, date)
+            self.eliminated_sheet.cell(row=self.row, column=1).value = toElim[0]
+            self.eliminated_sheet.cell(row=self.row, column=2).set_explicit_value(value=toElim[1], data_type=cell.Cell.TYPE_STRING)
+            self.row += 1
+            del self.eastTeams[toElim[2]]
         #writes the remaining teams as Playoffs
         for team in self.eastTeams:
             self.eliminated_sheet.cell(row=self.row, column=1).value = team['name']
